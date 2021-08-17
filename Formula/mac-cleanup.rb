6d3524d2733c172670d446ba36715ccd5ccd2fd3
Florian Wartner <fwartner@users.noreply.github.com>
2021-08-17 23:29:47 +0200

    Update mac-cleanup.rb

--- Formula/mac-cleanup.rb
+++ Formula/mac-cleanup.rb
@@ -1,8 +1,6 @@
 class MacCleanup < Formula
   desc "🗑️ cleanup script for macos"
   homepage "https://github.com/mac-cleanup/mac-cleanup"
-  url "https://github.com/fwartner/mac-cleanup/archive/v1.1.9.tar.gz"
-  sha256 "2d00ac048f6220fe23fb508c9f3a4a1d47cc1f8067fa22127c0f2e87a5b0182a"
   license "MIT"
 
   def install
@@ -10,6 +8,6 @@ class MacCleanup < Formula
   end
 
   test do
-    assert_match "🗑️  Cleanup script for macOS (by github.com/fwartner)r", shell_output("#{bin}/mac-cleanup -h", 2)
+    assert_match "🗑️  Cleanup script for macOS (by github.com/fwartner)", shell_output("#{bin}/mac-cleanup -h", 2)
   end
 end
